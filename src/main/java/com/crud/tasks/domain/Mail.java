fe18182e1b4175b1f4632fc30e7c91de89694509
PiotrOlchawa <piotr.olchawa@protonmail.ch>
2018-10-10 21:50:03 +0200

    zad 19.3

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,21 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Mail {
+    private String mailTo;
+    private String toCC;
+    private String subject;
+    private String message;
+
+    public Mail(String mailTo, String subject, String message) {
+        this.mailTo = mailTo;
+        this.subject = subject;
+        this.message = message;
+    }
+}
